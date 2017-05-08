09f52ef537996ff7283acbd477686bed67192dff
xiezehao <798351803@qq.com>
2017-05-08 17:40:03 +0800

    微信小程序
    微信小程序

---
+++ pages/textarea/textarea.js
@@ -0,0 +1,18 @@
+//textarea.js
+Page({
+  data: {
+    height: 20,
+    focus: false
+  },
+  bindButtonTap: function() {
+    this.setData({
+      focus: true
+    })
+  },
+  bindTextAreaBlur: function(e) {
+    console.log(e.detail.value)
+  },
+  bindFormSubmit: function(e) {
+    console.log(e.detail.value.textarea)
+  }
+})
